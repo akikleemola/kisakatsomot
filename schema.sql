0a9667c4c61c85b3bff9189dadfa405d625eb954
akikleemola <aki.kleemola@gmail.com>
2026-03-17 10:01:16 +0200

    Add new place form

--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+CREATE Table places(
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    address TEXT,
+    city TEXT,
+    description TEXT,
+    user_id INTEGER REFERENCES users
+);
